253b387a3d530314e027f0804a986af89417ded8
Charles Joshua Uy <charlesjoshuauy@gmail.com>
2021-02-03 10:21:48 +0800

    Oragnizing files and tabs logic

---
+++ assets/javascript/drag_and_drop.js
@@ -0,0 +1,18 @@
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("img", ev.target.id);
+  document.getElementById("piece").addEventListener("dragstart", function(e) {
+  var img = document.createElement("img");
+  img.src = "../images/invis.png";
+  e.dataTransfer.setDragImage(img, 0, 0);
+  }, false);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("img");
+  ev.target.appendChild(document.getElementById(data));
+}
