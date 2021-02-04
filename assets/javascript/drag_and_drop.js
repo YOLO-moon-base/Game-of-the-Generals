4a0a9113c61dcbc1aba3c51853e876ad26fd6e2f
toffee05 <75578897+toffee05@users.noreply.github.com>
2021-02-04 21:33:23 +0800

    Update drag_and_drop.js

--- assets/javascript/drag_and_drop.js
+++ assets/javascript/drag_and_drop.js
@@ -4,13 +4,13 @@ function allowDrop(ev) {
 
 function drag(ev) {
   ev.dataTransfer.setData("img", ev.target.id);
+}
   
  document.getElementById("piece").addEventListener("dragstart", function(ev) {
   var img = document.createElement("img");
   img.src = "../images/invis.png";
   e.dataTransfer.setDragImage(img, 0, 0);
   }, false);
-}
 
 function drop(ev) {
   ev.preventDefault();
